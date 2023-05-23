d60ab6a7bf2aa4beb8d9b98e24c9dd13996ef22d
Paul Cadman <git@paulcadman.dev>
2023-05-23 18:18:29 +0100

    Detect shell and modify PATH interactively

--- juvix-installer.sh
+++ juvix-installer.sh
@@ -7,6 +7,9 @@ set -u
 
 JUVIX_RELEASE_ROOT="https://github.com/anoma/juvix/releases"
 
+JUVIX_DIR=${XDG_DATA_HOME:=$HOME/.local/share}/juvix
+JUVIX_BIN=${XDG_BIN_HOME:=$HOME/.local/bin}
+
 usage() {
     cat <<EOF
 juvix-installer 0.1.0
@@ -21,10 +24,10 @@ main() {
     get_architecture
     need_cmd uname
     need_cmd mktemp
-    need_cmd chmod
     need_cmd mkdir
     need_cmd rm
     need_cmd rmdir
+    need_cmd tar
 
     get_architecture || return 1
     local _arch="$RETVAL"
@@ -36,14 +39,44 @@ main() {
     fi
     local _filename="juvix-${_arch}.tar.gz"
     local _file="${_dir}/${_filename}"
-
     local _url="${JUVIX_RELEASE_ROOT}/latest/download/${_filename}"
 
-    printf '%s\n' 'info: downloading juvix' 1>&2
+    find_shell_name
+    local _shell_name="$RETVAL"
+    find_profile_path
+    local _profile_path="$RETVAL"
 
-    ensure mkdir -p "$_dir"
-    ensure downloader "$_url" "$_file" "$_arch"
-    say "$_dir"
+    ask_profile "$_shell_name" "$_profile_path"
+    local _adjust_profile_answer="$RETVAL"
+
+    write_env
+    # printf '%s\n' 'info: downloading juvix' 1>&2
+    # ensure mkdir -p "$_dir"
+    # ensure downloader "$_url" "$_file" "$_arch"
+    # printf '%s\n' "info: copying juvix into ${JUVIX_BIN}" 1>&2
+    # ensure mkdir -p "${JUVIX_BIN}"
+    # ensure tar -xzf "$_file" -C ${JUVIX_BIN}
+    # ignore rm "$_file"
+    # ignore rmdir "$_dir"
+    #
+    adjust_profile "$_adjust_profile_answer" "$_shell_name" "$_profile_path"
+}
+
+
+# Writes a script to $JUVIX_DIR/env that, when sourced, prepends the
+# ${JUVIX_BIN} directory to $PATH if it is not already present there.
+write_env() {
+    ensure mkdir -p ${JUVIX_DIR}
+    local _env_file="${JUVIX_DIR}/env"
+    cat <<-EOF > "$_env_file" || err "Failed to create env file: $_env_file"
+case ":\$PATH:" in
+    *:"${JUVIX_BIN}":*)
+        ;;
+    *)
+        export PATH="${JUVIX_BIN}:\$PATH"
+        ;;
+esac
+EOF
 }
 
 get_architecture() {
@@ -132,10 +165,8 @@ ignore() {
 # use wget instead.
 downloader() {
     local _dld
-    local _ciphersuites
     local _err
     local _status
-    local _retry
     if check_cmd curl; then
         _dld=curl
     elif check_cmd wget; then
@@ -147,30 +178,8 @@ downloader() {
     if [ "$1" = --check ]; then
         need_cmd "$_dld"
     elif [ "$_dld" = curl ]; then
-        check_curl_for_retry_support
-        _retry="$RETVAL"
-        get_ciphersuites_for_curl
-        _ciphersuites="$RETVAL"
-        if [ -n "$_ciphersuites" ]; then
-            # $_retry must not be double-quoted
-            # shellcheck disable=SC2086
-            _err=$(curl $_retry --proto '=https' --tlsv1.2 --ciphers "$_ciphersuites" --silent --show-error --fail --location "$1" --output "$2" 2>&1)
-            _status=$?
-        else
-            echo "Warning: Not enforcing strong cipher suites for TLS, this is potentially less secure"
-            if ! check_help_for "$3" curl --proto --tlsv1.2; then
-                echo "Warning: Not enforcing TLS v1.2, this is potentially less secure"
-                # $_retry must not be double-quoted
-                # shellcheck disable=SC2086
-                _err=$(curl $_retry --silent --show-error --fail --location "$1" --output "$2" 2>&1)
-                _status=$?
-            else
-                # $_retry must not be double-quoted
-                # shellcheck disable=SC2086
-                _err=$(curl $_retry --proto '=https' --tlsv1.2 --silent --show-error --fail --location "$1" --output "$2" 2>&1)
-                _status=$?
-            fi
-        fi
+        _err=$(curl --proto '=https' --tlsv1.2 --silent --show-error --fail --location "$1" --output "$2" 2>&1)
+        _status=$?
         if [ -n "$_err" ]; then
             echo "$_err" >&2
             if echo "$_err" | grep -q 404$; then
@@ -179,28 +188,8 @@ downloader() {
         fi
         return $_status
     elif [ "$_dld" = wget ]; then
-        if [ "$(wget -V 2>&1|head -2|tail -1|cut -f1 -d" ")" = "BusyBox" ]; then
-            echo "Warning: using the BusyBox version of wget.  Not enforcing strong cipher suites for TLS or TLS v1.2, this is potentially less secure"
-            _err=$(wget "$1" -O "$2" 2>&1)
-            _status=$?
-        else
-            get_ciphersuites_for_wget
-            _ciphersuites="$RETVAL"
-            if [ -n "$_ciphersuites" ]; then
-                _err=$(wget --https-only --secure-protocol=TLSv1_2 --ciphers "$_ciphersuites" "$1" -O "$2" 2>&1)
-                _status=$?
-            else
-                echo "Warning: Not enforcing strong cipher suites for TLS, this is potentially less secure"
-                if ! check_help_for "$3" wget --https-only --secure-protocol; then
-                    echo "Warning: Not enforcing TLS v1.2, this is potentially less secure"
-                    _err=$(wget "$1" -O "$2" 2>&1)
-                    _status=$?
-                else
-                    _err=$(wget --https-only --secure-protocol=TLSv1_2 "$1" -O "$2" 2>&1)
-                    _status=$?
-                fi
-            fi
-        fi
+        _err=$(wget --https-only --secure-protocol=TLSv1_2 "$1" -O "$2" 2>&1)
+        _status=$?
         if [ -n "$_err" ]; then
             echo "$_err" >&2
             if echo "$_err" | grep -q ' 404 Not Found$'; then
@@ -209,151 +198,151 @@ downloader() {
         fi
         return $_status
     else
-        err "Unknown downloader"   # should not reach here
+        err "Unknown downloader"
     fi
 }
 
-# Check if curl supports the --retry flag, then pass it to the curl invocation.
-check_curl_for_retry_support() {
-    local _retry_supported=""
-    # "unspecified" is for arch, allows for possibility old OS using macports, homebrew, etc.
-    if check_help_for "notspecified" "curl" "--retry"; then
-        _retry_supported="--retry 3"
-        if check_help_for "notspecified" "curl" "--continue-at"; then
-            # "-C -" tells curl to automatically find where to resume the download when retrying.
-            _retry_supported="--retry 3 -C -"
-        fi
-    fi
-
-    RETVAL="$_retry_supported"
+find_shell_name() {
+    local _shell_name
+    case $SHELL in
+        */zsh)
+            _shell_name="zsh" ;;
+        */bash)
+            _shell_name="bash" ;;
+        */sh) # login shell is sh, but might be a symlink to bash or zsh
+            if [ -n "${BASH:-}" ] ; then
+                _shell_name="bash"
+            elif [ -n "${ZSH_VERSION:-}" ] ; then
+                _shell_name="zsh"
+            else
+                _shell_name=""
+            fi
+            ;;
+        */fish)
+            _shell_name="fish" ;;
+        *)
+            _shell_name="" ;;
+    esac
+    RETVAL="$_shell_name"
 }
 
-# Return cipher suite string specified by user, otherwise return strong TLS 1.2-1.3 cipher suites
-# if support by local tools is detected. Detection currently supports these curl backends:
-# GnuTLS and OpenSSL (possibly also LibreSSL and BoringSSL). Return value can be empty.
-get_ciphersuites_for_curl() {
-    local _openssl_syntax="no"
-    local _gnutls_syntax="no"
-    local _backend_supported="yes"
-    if curl -V | grep -q ' OpenSSL/'; then
-        _openssl_syntax="yes"
-    elif curl -V | grep -iq ' LibreSSL/'; then
-        _openssl_syntax="yes"
-    elif curl -V | grep -iq ' BoringSSL/'; then
-        _openssl_syntax="yes"
-    elif curl -V | grep -iq ' GnuTLS/'; then
-        _gnutls_syntax="yes"
-    else
-        _backend_supported="no"
-    fi
-
-    local _args_supported="no"
-    if [ "$_backend_supported" = "yes" ]; then
-        # "unspecified" is for arch, allows for possibility old OS using macports, homebrew, etc.
-        if check_help_for "notspecified" "curl" "--tlsv1.2" "--ciphers" "--proto"; then
-            _args_supported="yes"
-        fi
-    fi
-
-    local _cs=""
-    if [ "$_args_supported" = "yes" ]; then
-        if [ "$_openssl_syntax" = "yes" ]; then
-            _cs=$(get_strong_ciphersuites_for "openssl")
-        elif [ "$_gnutls_syntax" = "yes" ]; then
-            _cs=$(get_strong_ciphersuites_for "gnutls")
-        fi
-    fi
-
-    RETVAL="$_cs"
+find_profile_path() {
+    local _profile_path
+    case $SHELL in
+        */zsh)
+            if [ -n "${ZDOTDIR:-}" ]; then
+                _profile_file="$ZDOTDIR/.zshrc"
+            else
+                _profile_file="$HOME/.zshrc"
+            fi ;;
+        */bash)
+            _profile_file="$HOME/.bashrc" ;;
+        */sh)
+            if [ -n "${BASH:-}" ] ; then
+                _profile_file="$HOME/.bashrc"
+            elif [ -n "${ZSH_VERSION:-}" ] ; then
+                _profile_file="$HOME/.zshrc"
+            else
+                _profile_file=""
+            fi
+            ;;
+        */fish)
+            _profile_file="$HOME/.config/fish/config.fish" ;;
+        *) _profile_file="" ;;
+    esac
+    RETVAL="$_profile_file"
 }
 
-# Return cipher suite string specified by user, otherwise return strong TLS 1.2-1.3 cipher suites
-# if support by local tools is detected. Detection currently supports these wget backends:
-# GnuTLS and OpenSSL (possibly also LibreSSL and BoringSSL). Return value can be empty.
-get_ciphersuites_for_wget() {
-    local _cs=""
-    if wget -V | grep -q '\-DHAVE_LIBSSL'; then
-        # "unspecified" is for arch, allows for possibility old OS using macports, homebrew, etc.
-        if check_help_for "notspecified" "wget" "TLSv1_2" "--ciphers" "--https-only" "--secure-protocol"; then
-            _cs=$(get_strong_ciphersuites_for "openssl")
-        fi
-    elif wget -V | grep -q '\-DHAVE_LIBGNUTLS'; then
-        # "unspecified" is for arch, allows for possibility old OS using macports, homebrew, etc.
-        if check_help_for "notspecified" "wget" "TLSv1_2" "--ciphers" "--https-only" "--secure-protocol"; then
-            _cs=$(get_strong_ciphersuites_for "gnutls")
-        fi
+# Ask user if they want to adjust the shell profile.
+ask_profile() {
+    local _shell_name=$1
+    local _profile_path=$2
+    if [ -z "$_shell_name" ] ; then
+        RETVAL="noop"
+        return
     fi
 
-    RETVAL="$_cs"
+    local _profile_answer
+    local _profile_action
+
+    while true; do
+            echo "-------------------------------------------------------------------------------"
+            echo ""
+            echo "Detected $_shell_name shell on your system..."
+            echo "Do you want to automatically prepend the required PATH variable to \"${_profile_file}\"?"
+            echo ""
+            echo "[Y] Yes  [N] No  [?] Help (default is \"Y\")."
+            echo ""
+            read -r _profile_answer </dev/tty
+        case $_profile_answer in
+            [Yy]* | "")
+                _profile_action="adjust"
+                break
+                ;;
+            [Nn]*)
+                _profile_action="noop"
+                break
+                ;;
+
+            *)
+                ;;
+        esac
+    done
+    RETVAL="$_profile_action"
 }
 
-# Return strong TLS 1.2-1.3 cipher suites in OpenSSL or GnuTLS syntax. TLS 1.2
-# excludes non-ECDHE and non-AEAD cipher suites. DHE is excluded due to bad
-# DH params often found on servers (see RFC 7919). Sequence matches or is
-# similar to Firefox 68 ESR with weak cipher suites disabled via about:config.
-# $1 must be openssl or gnutls.
-get_strong_ciphersuites_for() {
-    if [ "$1" = "openssl" ]; then
-        # OpenSSL is forgiving of unknown values, no problems with TLS 1.3 values on versions that don't support it yet.
-        echo "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384"
-    elif [ "$1" = "gnutls" ]; then
-        # GnuTLS isn't forgiving of unknown values, so this may require a GnuTLS version that supports TLS 1.3 even if wget doesn't.
-        # Begin with SECURE128 (and higher) then remove/add to build cipher suites. Produces same 9 cipher suites as OpenSSL but in slightly different order.
-        echo "SECURE128:-VERS-SSL3.0:-VERS-TLS1.0:-VERS-TLS1.1:-VERS-DTLS-ALL:-CIPHER-ALL:-MAC-ALL:-KX-ALL:+AEAD:+ECDHE-ECDSA:+ECDHE-RSA:+AES-128-GCM:+CHACHA20-POLY1305:+AES-256-GCM"
-    fi
-}
+adjust_profile() {
+    local _profile_action=$1
+    local _shell_name=$2
+    local _profile_path=$3
 
-check_help_for() {
-    local _arch
-    local _cmd
-    local _arg
-    _arch="$1"
-    shift
-    _cmd="$1"
-    shift
+    echo $_profile_path
+    echo $_shell_name
 
-    local _category
-    if "$_cmd" --help | grep -q 'For all options use the manual or "--help all".'; then
-      _category="all"
-    else
-      _category=""
-    fi
-
-    case "$_arch" in
-
-        *macos*)
-        if check_cmd sw_vers; then
-            case $(sw_vers -productVersion) in
-                10.*)
-                    # If we're running on macOS, older than 10.13, then we always
-                    # fail to find these options to force fallback
-                    if [ "$(sw_vers -productVersion | cut -d. -f2)" -lt 13 ]; then
-                        # Older than 10.13
-                        echo "Warning: Detected macOS platform older than 10.13"
-                        return 1
-                    fi
+    case "$_profile_action" in
+        adjust)
+            case "$_shell_name" in
+                "")
+                    warn_path "Couldn't figure out login shell!"
+                    return
                     ;;
-                11.*)
-                    # We assume Big Sur will be OK for now
+                fish)
+                    mkdir -p "${_profile_path%/*}"
+                    sed -i -e '/# juvix-env$/ s/^#*/#/' "${_profile_path}"
+                    printf "\n%s" "set -gx PATH $JUVIX_BIN \$PATH # juvix-env" >> "${_profile_path}"
                     ;;
-                *)
-                    # Unknown product version, warn and continue
-                    echo "Warning: Detected unknown macOS major version: $(sw_vers -productVersion)"
-                    echo "Warning TLS capabilities detection may fail"
+                bash)
+                    sed -i -e '/# juvix-env$/ s/^#*/#/' "${_profile_path}"
+                    printf "\n%s" "[ -f \"${JUVIX_DIR}/env\" ] && source \"${JUVIX_DIR}/env\" # juvix-env" >> "${_profile_path}"
+                    ;;
+
+                zsh)
+                    sed -i -e '/# juvix-env$/ s/^#*/#/' "${_profile_path}"
+                    printf "\n%s" "[ -f \"${JUVIX_DIR}/env\" ] && source \"${JUVIX_DIR}/env\" # juvix-env" >> "${_profile_path}"
                     ;;
             esac
-        fi
-        ;;
-
+            echo
+            echo "==============================================================================="
+            echo
+            echo "OK! ${_profile_path} has been modified. Restart your terminal for the changes to take effect,"
+            echo "or type \"source ${JUVIX_DIR}/env\" to apply them in your current terminal session."
+            return
+            ;;
+        *)
+            warn_path ""
+            ;;
     esac
-
-    for _arg in "$@"; do
-        if ! "$_cmd" --help "$_category" | grep -q -- "$_arg"; then
-            return 1
-        fi
-    done
-
-    true # not strictly needed
 }
+
+warn_path() {
+    local _msg=$1
+    echo
+    echo "==============================================================================="
+    echo "$_msg"
+    echo "In order to run juvix, you need to adjust your PATH variable."
+    echo "To do so, you may want to run 'source $JUVIX_DIR/env' in your current terminal"
+    echo "session as well as your shell configuration (e.g. ~/.bashrc)."
+}
+
 
 main "$@" || exit 1
