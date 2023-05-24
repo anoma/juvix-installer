90c96bc4af82c9d9ccf08ee5157a5903b6071729
Paul Cadman <git@paulcadman.dev>
2023-05-24 16:48:28 +0100

    Include stubs in main file and explicitly use bash in test

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # shellcheck shell=dash
 
 set -u
-
-. stubs.sh
 
 # Setup temp directory
 BASE_TMP=$(mktemp -d)
@@ -11,6 +9,56 @@ trap 'rm -rf -- "$BASE_TMP"' EXIT
 
 # common stub parameters
 STUB_TAR=juvix.tar.gz
+
+die() { echo "$*" >&2; exit 2; }
+
+uname_stub() {
+    for arg in "$@"; do
+        OPTIND=1
+        while getopts :sm sub_arg "$arg"; do
+            case "$sub_arg" in
+                m)
+                    echo "$STUB_UNAME_M"
+                    exit 0
+                    ;;
+                s)
+                    echo "$STUB_UNAME_S"
+                    exit 0
+                    ;;
+                *)
+                    ;;
+            esac
+        done
+    done
+}
+
+curl_stub() {
+    local _output
+    local _location
+    while [ "$#" -gt 0 ]; do
+        case $1 in
+            --location)
+                _location="$2"
+                shift
+                shift
+                ;;
+            --output)
+                _output="$2"
+                shift
+                shift
+                ;;
+            *)
+                shift
+                ;;
+        esac
+    done
+    if [ -z "$_output" ]; then die "curl_stub: output missing"; fi
+    if [ -z "$_location" ]; then die "curl_stub: location missing"; fi
+    if ! [ "$EXPECTED_LOCATION" = "$_location" ]; then
+        die "curl was passed location: $_location, expected $EXPECTED_LOCATION"
+    fi
+    cp "$STUB_TAR" "$_output"
+}
 
 uname() {
     uname_stub "$@"
